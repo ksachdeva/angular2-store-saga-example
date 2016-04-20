1621159550caa18cb733f9968aa987bfc97f1d64
Kapil Sachdeva <ksachdeva17@gmail.com>
2016-04-20 15:59:46 -0500

    feat - add an example to make a real http GET to a son service

--- src/app/sagas/index.ts
+++ src/app/sagas/index.ts
@@ -3,6 +3,8 @@ import {Observable} from 'rxjs';
 import {createSaga} from 'store-saga';
 import {INCREMENT, DECREMENT} from './../actions';
 
+const BASE_WEB_URL = 'http://jsonplaceholder.typicode.com/';
+
 const increment = createSaga(function(){
   return iteration$ => iteration$
     .filter(iter => iter.action.type === DECREMENT)
@@ -38,7 +40,34 @@ const asyncEffect = createSaga(function sagaFactory(http: Http) {
 
 }, [Http]);
 
+
+const asyncEffect2 = createSaga(function sagaFactory(http: Http) {
+
+  return function loginSaga(iteration$: Observable<any>) {
+    return iteration$
+      .filter(iteration => iteration.action.type === DECREMENT)
+      .map(iteration => iteration.action.payload)
+      .mergeMap(payload => {
+        return http.get(BASE_WEB_URL + 'posts/1')
+          .map(res => {
+            return {
+              type: INCREMENT,
+              payload: res.json()
+            }
+          })
+          .catch(err => {
+            return Observable.of({
+              type: 'DECREMENT',
+              payload: err.json()
+            });
+          });
+      });
+  };
+
+}, [Http]);
+
 export default [
   // increment,
-  asyncEffect
+  // asyncEffect,
+  asyncEffect2
 ]
